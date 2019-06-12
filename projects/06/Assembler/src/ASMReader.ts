c5d42c44c9cdd75ae0a75e0856326190c1059f1e
Peter Szujewski <pszujewski@gmail.com>
2019-06-12 19:04:05 -0400

    Adds prettier

--- projects/06/Assembler/src/ASMReader.ts
+++ projects/06/Assembler/src/ASMReader.ts
@@ -1,33 +1,34 @@
 import * as fs from "fs";
 
 export default class ASMReader {
-  public async getTokens(path: string): Promise<string[]> {
-    //let tokens: string[];
-    let source: Buffer;
-    let rawFile: string;
+    public async getTokens(path: string): Promise<string[]> {
+        //let tokens: string[];
+        let source: Buffer;
+        let rawFile: string;
 
-    try {
-      source = await this.readFileAsync(path);
-      rawFile = source.toString();
-    } catch (err) {
-      throw err;
+        try {
+            source = await this.readFileAsync(path);
+            rawFile = source.toString();
+            const tokens: string[] = rawFile.split("\n");
+        } catch (err) {
+            throw err;
+        }
+        return [];
     }
-    return [];
-  }
 
-  private readFileAsync(path: string): Promise<Buffer> {
-    return new Promise<Buffer>(
-      (resolve, reject): void => {
-        fs.readFile(
-          path,
-          (err, data): void => {
-            if (err) {
-              reject(err);
+    private readFileAsync(path: string): Promise<Buffer> {
+        return new Promise<Buffer>(
+            (resolve, reject): void => {
+                fs.readFile(
+                    path,
+                    (err, data): void => {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve(data);
+                    }
+                );
             }
-            resolve(data);
-          }
         );
-      }
-    );
-  }
+    }
 }
--- projects/06/Assembler/test/Assembler.spec.ts
+++ projects/06/Assembler/test/Assembler.spec.ts
@@ -1,56 +1,56 @@
 import { expect } from "chai";
 import Assembler from "../src/Assembler";
 
-interface IASMInputTest {
-  path: string;
-  expectedTokens: string[];
+interface ASMInputTest {
+    path: string;
+    expectedTokens: string[];
 }
 
-const asmTests: IASMInputTest[] = [
-  {
-    path: `${__dirname}/asmExamples/Add.asm`,
-    expectedTokens: ["@2", "D=A", "@3", "D=D+A", "@0", "M=D"]
-  },
-  {
-    path: `${__dirname}/asmExamples/Max.asm`,
-    expectedTokens: [
-      "@R0",
-      "D=M",
-      "@R1",
-      "D=D-M",
-      "@OUTPUT_FIRST",
-      "D;JGT",
-      "@R1",
-      "D=M",
-      "@OUTPUT_D",
-      "0;JMP",
-      "(OUTPUT_FIRST)",
-      "@R0",
-      "D=M",
-      "(OUTPUT_D)",
-      "@R2",
-      "M=D",
-      "(INFINITE_LOOP)",
-      "@INFINITE_LOOP",
-      "0;JMP"
-    ]
-  }
+const asmTests: ASMInputTest[] = [
+    {
+        path: `${__dirname}/asmExamples/Add.asm`,
+        expectedTokens: ["@2", "D=A", "@3", "D=D+A", "@0", "M=D"],
+    },
+    {
+        path: `${__dirname}/asmExamples/Max.asm`,
+        expectedTokens: [
+            "@R0",
+            "D=M",
+            "@R1",
+            "D=D-M",
+            "@OUTPUT_FIRST",
+            "D;JGT",
+            "@R1",
+            "D=M",
+            "@OUTPUT_D",
+            "0;JMP",
+            "(OUTPUT_FIRST)",
+            "@R0",
+            "D=M",
+            "(OUTPUT_D)",
+            "@R2",
+            "M=D",
+            "(INFINITE_LOOP)",
+            "@INFINITE_LOOP",
+            "0;JMP",
+        ],
+    },
 ];
 
 describe("Assembler getASMTokens for a source ASM file", () => {
-  const assembler: Assembler = new Assembler();
-  let asmTest: IASMInputTest;
+    const assembler: Assembler = new Assembler();
+    let asmTest: ASMInputTest;
 
-  it("Should parse asm input into its tokens", async () => {
-    let result: string[];
+    it("Should parse asm input into its tokens", async () => {
+        let result: string[];
 
-    for (asmTest of asmTests) {
-      try {
-        result = await assembler.getASMTokens(asmTest.path);
-      } catch (err) {
-        result = err;
-      }
-      expect(result).to.equal(asmTest.expectedTokens);
-    }
-  });
+        for (asmTest of asmTests) {
+            try {
+                result = await assembler.getASMTokens(asmTest.path);
+            } catch (err) {
+                result = err;
+            }
+            expect(result).to.equal(asmTest.expectedTokens);
+        }
+    });
 });
