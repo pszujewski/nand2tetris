a85819cb759e2be94454dbf23eda1f5335d15344
Peter Szujewski <peter@abovethetreeline.com>
2019-06-12 08:31:44 -0400

    Completes first test

--- projects/06/Assembler/test/Assembler.spec.ts
+++ projects/06/Assembler/test/Assembler.spec.ts
@@ -1,13 +1,56 @@
 import { expect } from "chai";
 import Assembler from "../src/Assembler";
 
-describe("Assembler getASMTokens for a source ASM file", () => {
-    const assembler: Assembler = new Assembler();
-    
-    it("Should parse Max.asm into its tokens", async () => {
-        const path = `${__dirname}/asmExamples/Max.asm`;
-        let result: string[];
+interface IASMInputTest {
+  path: string;
+  expectedTokens: string[];
+}
 
-        result = await assembler.getASMTokens(path);
-    });
+const asmTests: IASMInputTest[] = [
+  {
+    path: `${__dirname}/asmExamples/Add.asm`,
+    expectedTokens: ["@2", "D=A", "@3", "D=D+A", "@0", "M=D"]
+  },
+  {
+    path: `${__dirname}/asmExamples/Max.asm`,
+    expectedTokens: [
+      "@R0",
+      "D=M",
+      "@R1",
+      "D=D-M",
+      "@OUTPUT_FIRST",
+      "D;JGT",
+      "@R1",
+      "D=M",
+      "@OUTPUT_D",
+      "0;JMP",
+      "(OUTPUT_FIRST)",
+      "@R0",
+      "D=M",
+      "(OUTPUT_D)",
+      "@R2",
+      "M=D",
+      "(INFINITE_LOOP)",
+      "@INFINITE_LOOP",
+      "0;JMP"
+    ]
+  }
+];
+
+describe("Assembler getASMTokens for a source ASM file", () => {
+  const assembler: Assembler = new Assembler();
+  let asmTest: IASMInputTest;
+
+  it("Should parse asm input into its tokens", async () => {
+    let result: string[];
+
+    for (asmTest of asmTests) {
+      try {
+        result = await assembler.getASMTokens(asmTest.path);
+      } catch (err) {
+        result = err;
+      }
+      expect(result).to.equal(asmTest.expectedTokens);
+    }
+  });
 });
