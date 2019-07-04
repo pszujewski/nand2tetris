cdbb1d323b74e27f05a244091ed4a9d0f7be5b7b
Peter Szujewski <pszujewski@gmail.com>
2019-07-04 14:51:33 -0400

    Project setup complete

--- projects/07/VMTranslator/lib/VMTranslator.js
+++ projects/07/VMTranslator/lib/VMTranslator.js
@@ -1,36 +1,13 @@
-"use strict";
+"use strict";var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports, "__esModule", { value: true });exports["default"] = void 0;var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));var VMTranslator = /*#__PURE__*/function () {function VMTranslator() {(0, _classCallCheck2["default"])(this, VMTranslator);(0, _defineProperty2["default"])(this, "translateCommandToHack",
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
 
-var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 
-var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 
-var VMTranslator =
-/*#__PURE__*/
-function () {
-  function VMTranslator() {
-    (0, _classCallCheck2["default"])(this, VMTranslator);
-  }
 
-  (0, _createClass2["default"])(VMTranslator, [{
-    key: "tokenize",
-    value: function tokenize(rawVmSourceCode) {
+
+
+    function () {
       return null;
-    }
-  }, {
-    key: "translateToHackAssembly",
-    value: function translateToHackAssembly(vmTokens) {
-      return null;
-    }
-  }]);
-  return VMTranslator;
-}();
-
-exports["default"] = VMTranslator;
+    });}(0, _createClass2["default"])(VMTranslator, [{ key: "tokenize", value: function tokenize() {return null;} }, { key: "translateToHackAssembly", value: function translateToHackAssembly() {return null;} }]);return VMTranslator;}();exports["default"] = VMTranslator;
 //# sourceMappingURL=VMTranslator.js.map
