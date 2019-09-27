df749e050b7d1c0e9766fad9f738384371ffefb3
Peter Szujewski <pszujewski@gmail.com>
2019-09-26 20:38:32 -0400

    Tokenizer class

---
+++ projects/10/JackAnalyzer/src/JackTokenizer/Tokens.ts
@@ -0,0 +1,11 @@
+export default class Tokens {
+    private baseTokens: string[];
+
+    constructor(baseTokens: string[]) {
+        this.baseTokens = baseTokens;
+    }
+
+    public parse(): string[] {
+        //const chunks: string[][] = this.baseTokens.map((bt: string): string[] => {})
+    }
+}
