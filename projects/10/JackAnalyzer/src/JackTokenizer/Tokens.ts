649cbb29977140a2906d68f45428e6b1a37af9e9
Peter Szujewski <pszujewski@gmail.com>
2019-09-29 18:09:55 -0400

    Working

--- projects/10/JackAnalyzer/src/JackTokenizer/Tokens.ts
+++ projects/10/JackAnalyzer/src/JackTokenizer/Tokens.ts
@@ -1,3 +1,5 @@
+import SymbolTable from "../SymbolTable";
+
 export default class Tokens {
     private baseTokens: string[];
 
@@ -6,6 +8,36 @@ export default class Tokens {
     }
 
     public parse(): string[] {
-        //const chunks: string[][] = this.baseTokens.map((bt: string): string[] => {})
+        const chunks: string[][] = this.baseTokens.map(this.parseOne);
     }
+
+    private parseOne = (bt: string): string[] => {
+        const tokens: string[] = [];
+        const splitBt: string[] = bt.split("s");
+
+        let i: number;
+        let j: number;
+
+        let initial: string;
+        let nextToken: string;
+
+        for (i = 0; i < splitBt.length; i++) {
+            initial = splitBt[i];
+            nextToken = "";
+
+            for (j = 0; j < initial.length; j++) {
+                const char: string = initial[j];
+
+                if (nextToken && SymbolTable.isSymbol(char)) {
+                    tokens.push(nextToken.trim());
+                    nextToken = "";
+                    tokens.push(char);
+                } else {
+                    nextToken = nextToken + char;
+                }
+            }
+        }
+
+        return tokens;
+    };
 }
---
+++ projects/10/JackAnalyzer/src/SymbolTable/index.ts
@@ -0,0 +1,29 @@
+import Symbol from "../../types/Symbol";
+
+const symbols: string[] = [
+    Symbol.CurlyRight,
+    Symbol.CurlyLeft,
+    Symbol.ParenRight,
+    Symbol.ParenLeft,
+    Symbol.BracketRight,
+    Symbol.BracketLeft,
+    Symbol.Period,
+    Symbol.Comma,
+    Symbol.Semi,
+    Symbol.Plus,
+    Symbol.Minus,
+    Symbol.Times,
+    Symbol.SlashRight,
+    Symbol.Ampersand,
+    Symbol.Pipe,
+    Symbol.LessThan,
+    Symbol.GreaterThan,
+    Symbol.Equals,
+    Symbol.Not,
+];
+
+export default class SymbolTable {
+    static isSymbol(char: string): boolean {
+        return symbols.includes(char);
+    }
+}
