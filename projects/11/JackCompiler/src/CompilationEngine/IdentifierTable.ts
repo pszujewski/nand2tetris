65c59b6468d74a3ecf5763e42cabb1a6eddbdd17
Peter Szujewski <peter@abovethetreeline.com>
2019-12-09 08:30:48 -0500

    Begins adding

--- projects/11/JackCompiler/src/CompilationEngine/IdentifierTable.ts
+++ projects/11/JackCompiler/src/CompilationEngine/IdentifierTable.ts
@@ -1,5 +1,44 @@
+import Scope, { Identifier, VariableKind } from "../../types/Scope";
+
 /**
  * Should match API for SymbolTable on page pg. 239
  */
 
-export default class IdentifierTable {}
+export default class IdentifierTable {
+    private scope: Scope;
+
+    constructor() {
+        this.scope = {
+            classLevel: {
+                field: [],
+                static: [],
+            },
+            subroutineLevel: {
+                varLocal: [],
+                argument: [],
+            },
+        };
+    }
+
+    /**
+     * Starts a new subroutine scope (resets the subroutine's symbol table)
+     */
+    public startSubroutine() {
+        this.scope.subroutineLevel = {
+            varLocal: [],
+            argument: [],
+        };
+    }
+
+    public define(identifier: Identifier) {
+        switch (identifier.kind) {
+            case VariableKind.FIELD:
+                this.scope.classLevel.field.push(identifier);
+                break;
+            case VariableKind.STATIC:
+                this.scope.classLevel.static.push(identifier);
+            default:
+                throw new Error("Invalid variable kind" + identifier.kind);
+        }
+    }
+}
---
+++ projects/11/JackCompiler/types/Scope.ts
@@ -0,0 +1,29 @@
+export default interface Scope {
+    classLevel: ClassScope;
+    subroutineLevel: SubroutineScope;
+}
+
+export interface ClassScope {
+    field: Identifier[];
+    static: Identifier[];
+}
+
+export interface SubroutineScope {
+    varLocal: Identifier[];
+    argument: Identifier[];
+}
+
+// The ID corresponds to the index position of the identifer in its array
+
+export interface Identifier {
+    name: string;
+    type: string;
+    kind: VariableKind;
+}
+
+export enum VariableKind {
+    ARG = "ARG",
+    VAR = "VAR",
+    STATIC = "STATIC",
+    FIELD = "FIELD",
+}
