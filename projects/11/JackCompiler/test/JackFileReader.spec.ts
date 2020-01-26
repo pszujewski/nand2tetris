53a2f93993371401fe6de068cabf1d43bf1e14a4
Peter Szujewski <pszujewski@gmail.com>
2020-01-25 21:20:50 -0500

    Done with 11

--- projects/11/JackCompiler/test/JackFileReader.spec.ts
+++ projects/11/JackCompiler/test/JackFileReader.spec.ts
@@ -23,7 +23,7 @@ describe("JackFileReader", () => {
         let JA: JackAnalyzer;
 
         try {
-            JA = new JackAnalyzer("../../../Pong");
+            JA = new JackAnalyzer("../../../Seven");
             await JA.analyze();
         } catch (err) {
             console.log(err.message);
